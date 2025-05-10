a789b256e5628067846857f931fc498cea3f7608
wootaiklee <fined000@naver.com>
2025-05-10 18:13:04 +0900

    feat: Singleton Pattern

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -20,4 +20,13 @@ public class SingletonTest {
         System.out.println(memberService1);
         System.out.println(memberService2);
     }
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println(singletonService1);
+        System.out.println(singletonService2);
+    }
 }
